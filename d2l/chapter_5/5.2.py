bb53d9cc36d689fe6125f51d9a9bba0ec0bfbf8b
zero-robotic <Jonica8499@hotmail.com>
2026-03-02 13:28:42 +0800

    feat(d2l): Update 5.2.py and add 5.4.py

--- d2l/chapter_5/5.2.py
+++ d2l/chapter_5/5.2.py
@@ -32,3 +32,64 @@ print(rgnet)
 
 
 print(rgnet[0][1][0].bias.data)
+
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+
+
+print(net[0].weight.data[0], net[0].bias.data[0])
+net.apply(init_normal)
+print(net[0].weight.data[0], net[0].bias.data[0])
+
+
+def init_constant(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
+net.apply(init_constant)
+print(net[0].weight.data[0], net[0].bias.data[0])
+
+
+def init_xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+
+
+def init_42(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 42)
+
+net[0].apply(init_xavier)
+net[2].apply(init_42)
+print(net[0].weight.data[0])
+print(net[2].weight.data)
+
+
+def my_init(m):
+    if type(m) == nn.Linear:
+        print("Init", *[(name, param.shape) for name, param in m.named_parameters()][0])
+        nn.init.uniform_(m.weight, -10, 10)
+        m.weight.data *= m.weight.data.abs() >= 5
+
+net.apply(my_init)
+print(net[0].weight[:2])
+
+net[0].weight.data[:] += 1
+net[0].weight.data[0, 0] = 42
+print(net[0].weight.data)
+
+
+# 我们需要给共享层一个名称，以便可以引用它的参数
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                    shared, nn.ReLU(),
+                    shared, nn.ReLU(),
+                    nn.Linear(8, 1))
+net(X)
+# 检查参数是否相同
+print(net[2].weight.data[0] == net[4].weight.data[0])
+net[2].weight.data[0, 0] = 100
+print(net[2].weight.data[0] == net[4].weight.data[0])
